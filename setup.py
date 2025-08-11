9d535014e6a7c81d09e33898d4e510e81e92346d
dulminahxxx <dulminahettiarachchi80@gmail.com>
2025-08-11 19:48:07 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Dulmina Nirwan',
+    author_email= 'dulmina.20220985@iit.ac.lk',
+    packages= find_packages(),
+    install_requires = []
+
+)
